d59a2f1048fbde7b765b74103f9d924df52e7151
andar1216 <ingramaj26@gmail.com>
2025-08-31 23:57:35 -0400

    trigger deploy 3

--- server.js
+++ server.js
@@ -146,10 +146,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// -----------------
-// Boot server
-// -----------------
-const PORT = process.env.PORT || 5252;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
